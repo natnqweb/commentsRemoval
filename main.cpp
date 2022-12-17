ba44c3d0d96b965d8252f6ca4e550488f0eba3ec
Natan Lisowski <82398767+natnqweb@users.noreply.github.com>
2022-12-17 17:36:10 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,17 +5,20 @@
 #include <vector>
 #include <algorithm>
 #include <regex>
-static std::vector<std::regex> vec_user_regexes{100};
-static std::vector<std::string> vec_excluded_files{ 100 };
-static std::vector<std::string> vec_edited_files{ 100 };
-static std::vector<std::regex> vec_line_regexes{ 100 };
-
+#include <execution>
+#include <chrono>
+constexpr auto INITIAL_RESERVE_SIZE = 10000;
+static std::vector<std::regex>  vec_user_regexes{};
+static std::vector<std::string> vec_excluded_files{};
+static std::vector<std::string> vec_edited_files{};
+static std::vector<std::regex>  vec_line_regexes{};
+long long number_of_lines_of_code = 0;
 static bool is_forbidden_path(const std::filesystem::path& path)
 {
 	if (vec_user_regexes.empty())
 		return false;
 
-	const auto rgx_it = std::find_if(std::begin(vec_user_regexes), std::end(vec_user_regexes), [dir = path.string().c_str()](const std::regex& rgx)
+	const auto rgx_it = std::find_if(std::execution::par, std::begin(vec_user_regexes), std::end(vec_user_regexes), [dir = path.string().c_str()](const std::regex& rgx)
 		{
 			std::cmatch m;
 			if (std::regex_match(dir, m, rgx))
@@ -38,7 +41,7 @@ static bool is_forbidden_line(const std::string& line)
 	if (vec_line_regexes.empty())
 		return false;
 
-	const auto it = std::find_if(std::begin(vec_line_regexes), std::end(vec_line_regexes), [a_line = line.c_str()](const std::regex& rgx)
+	const auto it = std::find_if(std::execution::par, std::begin(vec_line_regexes), std::end(vec_line_regexes), [a_line = line.c_str()](const std::regex& rgx)
 		{
 			std::cmatch m;
 			return std::regex_match(a_line, m, rgx);
@@ -59,9 +62,9 @@ static void remove_comments(const std::string& file_path) {
 	bool in_line_comment = false;
 
 	while (std::getline(input_file, line)) {
-		
+		number_of_lines_of_code++;
 		const bool is_excluded = is_forbidden_line(line);
-		
+
 		for (size_t i = 0; i < line.size(); i++) {
 			// Check for start of block comment
 			if (!is_excluded && line[i] == '/' && line[i + 1] == '*') {
@@ -95,6 +98,7 @@ static void remove_comments(const std::string& file_path) {
 		// Add a newline character after each line
 		output += '\n';
 	}
+
 	vec_edited_files.push_back(file_path);
 	// Write the output to the file
 	std::ofstream output_file(file_path);
@@ -139,11 +143,17 @@ static void extract_all_file_line_regexes(const std::filesystem::path& file_filt
 	extract_from_file(file_filter_path, &vec_line_regexes);
 }
 
-int main() 
+int main()
 {
 	auto currPath = std::filesystem::current_path();
 	std::string filter_path_str = currPath.string() + "\\filter_file.txt";
 	std::string line_regex_path_str = currPath.string() + "\\line_regexes.txt";
+
+	vec_excluded_files.reserve(INITIAL_RESERVE_SIZE);
+	vec_edited_files.reserve(INITIAL_RESERVE_SIZE);
+	vec_line_regexes.reserve(INITIAL_RESERVE_SIZE);
+	vec_user_regexes.reserve(INITIAL_RESERVE_SIZE);
+
 
 	std::cout << "config files:\n" << filter_path_str << "\n" << line_regex_path_str << "\n";
 	std::cout << "are you sure you want to remove all comments? y/n" << "\n";
@@ -157,29 +167,34 @@ int main()
 	std::cout << "enter directory for recursive cleanup : ";
 	std::cin >> directory_path;
 	std::cout << "\n";
-
+	auto start = std::chrono::high_resolution_clock::now();
 	auto filter_path = std::filesystem::path(filter_path_str);
 	auto line_regex_path = std::filesystem::path(line_regex_path_str);
 	extract_all_filters(filter_path);
 	extract_all_file_line_regexes(line_regex_path);
-	if(std::filesystem::exists(directory_path))
+	if (std::filesystem::exists(directory_path))
 		scan_directory(directory_path);
+
+	// Stop the timer
+	auto end = std::chrono::high_resolution_clock::now();
+	auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+	
 	std::cout << "all excluded files:\n";
 
-	std::for_each(vec_excluded_files.begin(), vec_excluded_files.end(), [](const auto& file)
-		{
-			std::cout << file << "\n"; 
-		});
-
-
-	std::cout << "all edited files:\n";
-
-	std::for_each(vec_edited_files.begin(), vec_edited_files.end(), [](const auto& file)
+	std::for_each(std::execution::seq, std::begin(vec_excluded_files), std::end(vec_excluded_files), [](const auto& file)
 		{
 			std::cout << file << "\n";
 		});
 
-	std::cout << "success";
+	std::cout << "all edited files:\n";
+
+	std::for_each(std::execution::seq, std::begin(vec_edited_files), std::end(vec_edited_files), [](const auto& file)
+		{
+			std::cout << file << "\n";
+		});
+
+	std::cout << "success, operation time : " << elapsed_time<< "ms";
+	std::cout << "\nnumber of lines of code touched: " << number_of_lines_of_code;
 	int retval = 0;
 	std::cin >> retval;
 	return retval;
